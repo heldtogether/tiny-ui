36eba861aba61a5770c52ad88d0d0abd65a7a836
Josh Sephton <josh.sephton@gmail.com>
2025-03-26 10:02:11 +0000

    feat: set custom font

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
 
 export function Portal({ children }: { children: ReactNode }) {
-  return <main className="tu:flex-1 tu:overflow-y-auto tu:p-4">{children}</main>;
+  return <main className="tu:flex-1 tu:overflow-y-auto tu:p-4 tu:font-sans">{children}</main>;
 }
--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -3,5 +3,5 @@ import React, { ReactNode } from "react";
 type ShellProps = { children?: ReactNode };
 
 export function Shell({ children }: ShellProps) {
-  return <div className="tu:flex tu:flex-col tu:h-screen tu:overflow-hidden">{children}</div>;
+  return <div className="tu:flex tu:flex-col tu:h-screen tu:overflow-hidden tu:font-space">{children}</div>;
 }
