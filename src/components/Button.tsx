b3bcb99d5bb1c88ba4077f69614dc417eacd427d
Josh Sephton <josh.sephton@gmail.com>
2025-03-27 09:15:24 +0000

    feat: add active state to buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,8 +22,8 @@ export function Button({
       return (
         <button
           className={`tu:py-2 tu:pl-4 ${iconOnly ? "tu:pr-2" : "tu:pr-12"} tu:flex-1 tu:cursor-pointer tu:flex tu:flex-row tu:items-center ${ghost
-            ? "tu:bg-transparent tu:text-blue-600 tu:hover:bg-blue-600 tu:hover:text-white"
-            : " tu:bg-blue-600 tu:text-white tu:hover:bg-blue-700 tu:hover:text-white"
+            ? "tu:bg-transparent tu:text-blue-600 tu:hover:bg-blue-600 tu:hover:text-white tu:active:bg-blue-700"
+            : " tu:bg-blue-600 tu:text-white tu:hover:bg-blue-700 tu:hover:text-white tu:active:bg-blue-800"
             }`}
           onClick={onClick}
         >
@@ -35,8 +35,8 @@ export function Button({
       return (
         <button
           className={`tu:py-2 tu:pl-4 ${iconOnly ? "tu:pr-2" : "tu:pr-12"} tu:flex-1 tu:cursor-pointer tu:flex tu:flex-row tu:items-center ${ghost
-            ? "tu:bg-transparent tu:text-gray-700 tu:hover:bg-gray-700 tu:hover:text-gray-100"
-            : "tu:bg-gray-700 tu:text-gray-100 tu:hover:bg-gray-900"
+            ? "tu:bg-transparent tu:text-gray-700 tu:hover:bg-gray-700 tu:hover:text-gray-100 tu:active:bg-gray-800"
+            : "tu:bg-gray-700 tu:text-gray-100 tu:hover:bg-gray-900 tu:active:bg-black"
             }`}
           onClick={onClick}
         >
@@ -48,8 +48,8 @@ export function Button({
       return (
         <button
           className={`tu:py-2 tu:pl-4 ${iconOnly ? "tu:pr-2" : "tu:pr-12"} tu:flex-1 tu:cursor-pointer tu:flex tu:flex-row tu:items-center ${ghost
-            ? "tu:bg-transparent tu:text-blue-500 tu:hover:border tu:hover:border-blue-500 tu:hover:bg-blue-50"
-            : "tu:border tu:border-blue-500 tu:text-blue-500 tu:hover:bg-blue-600 tu:hover:text-white"
+            ? "tu:bg-transparent tu:text-blue-500 tu:hover:border tu:hover:border-blue-500 tu:hover:bg-blue-50 tu:active:bg-blue-100"
+            : "tu:border tu:border-blue-500 tu:text-blue-500 tu:hover:bg-blue-600 tu:hover:text-white tu:active:bg-blue-700"
             }`}
           onClick={onClick}
         >
@@ -61,8 +61,8 @@ export function Button({
       return (
         <button
           className={`tu:py-2 tu:pl-4 ${iconOnly ? "tu:pr-2" : "tu:pr-12"} tu:flex-1 tu:cursor-pointer tu:flex tu:flex-row tu:items-center ${ghost
-            ? "tu:bg-transparent tu:text-red-700 tu:hover:bg-red-800 tu:hover:text-white"
-            : "tu:bg-red-700 tu:text-white tu:hover:bg-red-800 tu:hover:text-white"
+            ? "tu:bg-transparent tu:text-red-700 tu:hover:bg-red-700 tu:hover:text-white tu:active:bg-red-900"
+            : "tu:bg-red-700 tu:text-white tu:hover:bg-red-800 tu:hover:text-white tu:active:bg-red-900"
             }`}
           onClick={onClick}
         >
