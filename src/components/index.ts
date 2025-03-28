339c53ba9edcac101cf8bff4ee0dde72e47f065a
Josh Sephton <josh.sephton@gmail.com>
2025-03-28 16:21:39 +0000

    chore: ensure all components are exported

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,23 @@
 // Export all components
-export { Accordion } from './Accordion';
-export { Button } from './Button';
-export { Content } from './Content';
-export { ContentSwitcher } from './ContentSwitcher';
-export { Header } from './Header';
-export { Heading } from './Heading';
-export { Modal } from './Modal';
-export { Portal } from './Portal';
-export { Shell } from './Shell';
-export { Sidebar } from './Sidebar';
-export { Tooltip } from './Tooltip';
-export { TreeView } from './TreeView'; 
+export {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "./Accordion";
+export { Button } from "./Button";
+export { Content } from "./Content";
+export { ContentSwitcher, ContentSwitcherItem } from "./ContentSwitcher";
+export { Header, HeaderTitle, HeaderActions, HeaderAction } from "./Header";
+export { Heading } from "./Heading";
+export { Modal } from "./Modal";
+export { Portal } from "./Portal";
+export { Shell } from "./Shell";
+export {
+  Sidebar,
+  SidebarButtonItem,
+  SidebarDivider,
+  SidebarTextItem,
+} from "./Sidebar";
+export { Tooltip, TooltipContent } from "./Tooltip";
+export { TreeView, TreeNode } from "./TreeView";
